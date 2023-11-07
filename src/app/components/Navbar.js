977404cf2edeead6556f3bc9f420a80d88f2dc49
Duncan Burgess <d.q.burgess99@gmail.com>
2023-11-06 17:21:30 -0800

    first commit

---
+++ src/app/components/Navbar.js
@@ -0,0 +1,34 @@
+const menuItems = [
+  {
+    title: 'Home',
+    href: '/',
+  },
+  {
+    title: 'About',
+    href: '/about',
+  },
+  {
+    title: 'Contact',
+    href: '/contact',
+  },
+  {
+    title: 'Services',
+    href: '/services',
+  },
+  {
+    title: 'Products',
+    href: '/products',
+  },
+]
+
+export default () => {
+  return (
+    <div className="flex justify-end gap-4 m-6 text-red-600">
+      {menuItems.map((item) => (
+        <a href={item.href} className="px-4 py-2 bg-gray-100 shadow-md rounded-md">
+          {item.title}
+        </a>
+      ))}
+    </div>
+  )
+}
---
+++ src/app/components/PageTitle.js
@@ -0,0 +1,3 @@
+export default function PageTitle({ title }) {
+  return <h1 className="text-2xl font-bold mb-4 text-green-600 bg-red-100">{title}</h1>
+}
