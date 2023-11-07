0e1caafd326d793001b2ff5ba02eb7c94d698828
zeig101 <d.q.burgess99@gmail.com>
2023-11-06 18:06:43 -0800

    fix lint errors

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -21,11 +21,11 @@ const menuItems = [
   },
 ]
 
-export default () => {
+export default function Navbar() {
   return (
     <div className="flex justify-end gap-4 m-6 text-red-600">
-      {menuItems.map((item) => (
-        <a href={item.href} className="px-4 py-2 bg-gray-100 shadow-md rounded-md">
+      {menuItems.map((item, index) => (
+        <a key={index} href={item.href} className="px-4 py-2 bg-gray-100 shadow-md rounded-md">
           {item.title}
         </a>
       ))}
